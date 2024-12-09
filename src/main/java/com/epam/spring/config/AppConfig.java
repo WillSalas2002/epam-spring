af122a923f704569b0d5c10b77d579477ff1eb71
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-12-09 11:18:34 +0500

    add configuration components

---
+++ src/main/java/com/epam/spring/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.epam.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.epam.spring")
+public class AppConfig {
+}
