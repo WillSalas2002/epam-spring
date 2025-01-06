d96e8aed868779c8602071e44834da63e197529f
Will Salas <batir2jumatov@gmail.com>
2025-01-06 16:03:30 +0500

    fix: remove unnecessary code

--- src/main/java/com/epam/spring/config/AppConfig.java
+++ src/main/java/com/epam/spring/config/AppConfig.java
@@ -1,38 +1,9 @@
 package com.epam.spring.config;
 
-import com.epam.spring.model.Trainee;
-import com.epam.spring.model.Trainer;
-import com.epam.spring.model.Training;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 @Configuration
 @ComponentScan("com.epam.spring")
 public class AppConfig {
-
-    @Bean
-    public Map<Long, Trainee> traineeStorage() {
-        return new HashMap<>();
-    }
-
-    @Bean
-    public Map<Long, Trainer> trainerStorage() {
-        return new HashMap<>();
-    }
-
-    @Bean
-    public Map<Long, Training> trainingStorage() {
-        return new HashMap<>();
-    }
-
-    @Bean
-    public Set<String> usernameStorage() {
-        return new HashSet<>();
-    }
 }
