6d2b13bcc8e43398b9b343c6ca577c7d67bf7e66
Ilkham Jumatov <batir2jumatov@gmail.com>
2025-01-22 12:41:43 +0500

    chore: remove unused code

--- src/main/java/com/epam/spring/repository/TrainingTypeOperationsRepository.java
+++ src/main/java/com/epam/spring/repository/TrainingTypeOperationsRepository.java
@@ -2,7 +2,6 @@ package com.epam.spring.repository;
 
 import com.epam.spring.model.TrainingType;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
